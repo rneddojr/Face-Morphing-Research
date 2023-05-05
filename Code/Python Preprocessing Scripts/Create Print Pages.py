fbe48675044757836ddf8b95f97fdbf5e437c06e
rneddojr <124090051+rneddojr@users.noreply.github.com>
2023-05-05 09:59:54 -0400

    Make Pages for Printing

--- Code/Python Preprocessing Scripts/Create Print Pages.py
+++ Code/Python Preprocessing Scripts/Create Print Pages.py
@@ -4,11 +4,11 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-fontRoot = r"C://Users//rebel//Desktop//Research_with_Liu//FRLL and London Dataset//Font//aver-font//Aver-opKo.ttf"
+fontRoot = r"C://Users//xxxx//Font//aver-font//Aver-opKo.ttf"
 
-morphRoot = r"C:\Users\rebel\Downloads\morphs\feret\morph_diffusionC//"
-morphPageRoot = r"C:\Users\rebel\Downloads\morphs//FERET Diff Pages//"
-pageName = "FERET_DiffusionC_Print_Page_"
+imgRoot = r"C://Users//xxxx//Dataset//imgs//"
+printPageRoot = r"C://Users//xxxx//Dataset_Print_Pages//"
+pageName = "Dataset_Print_Page_"
 
 imageHeight = 600
 imageWidth = 600
@@ -18,19 +18,19 @@ tpMargin = 180
 font = ImageFont.truetype(fontRoot, 52)
 
 
-def get_morph_pages(file_count):
-    morphPages = file_count / 12
-    morphPages = math.ceil(morphPages) + 1
-    return morphPages
+def get_print_pages(file_count):
+    imgPages = file_count / 12
+    imgPages = math.ceil(imgPages) + 1
+    return imgPages
 
 
-def make_morph_pages(morphPages):
+def make_print_pages(imgPages):
     pageCountTwo = 1
 
-    for morphPages in range(1, morphPages):
+    for imgPages in range(1, imgPages):
         pageCountStringTwo = str(pageCountTwo)
         fullTwo = pageCountStringTwo.zfill(2)
-        Image_Rename_Two = morphPageRoot + pageName + fullTwo + ".png"
+        Image_Rename_Two = printPageRoot + pageName + fullTwo + ".png"
         imgb = Image.new("RGB", (2550, 3300), (255, 255, 255))
         imgb.save(Image_Rename_Two, 'PNG')
         pageCountTwo = pageCountTwo + 1
@@ -38,29 +38,29 @@ def make_morph_pages(morphPages):
 
 
 def pasteImages(counter, counterTwo, pageCounter, filename, file_extension):
-    img2 = Image.open(morphRoot + filename + file_extension)
+    img2 = Image.open(imgRoot + filename + file_extension)
     saveType = ".png"
     temp = str(math.ceil(pageCounter / 4))
     pageCounterStringTwo = temp
     fulltwo = pageCounterStringTwo.zfill(2)
 
-    img6 = Image.open(morphPageRoot + pageName + fulltwo + saveType)
+    img6 = Image.open(printPageRoot + pageName + fulltwo + saveType)
     draw = ImageDraw.Draw(img6)
 
     if counter == 1:
         img6.paste(img2, (lrMargin * counter + imageWidth * (counter - 1), tpMargin * counterTwo + imageHeight * (counterTwo - 1)))
-        img6.save((morphPageRoot + pageName + fulltwo + saveType), 'PNG', quality=100)
+        img6.save((printPageRoot + pageName + fulltwo + saveType), 'PNG', quality=100)
         if counterTwo == 1:
-            draw.text((2200, 3200), pageCounterStringTwo + " FERET DC", (0, 0, 0), font=font)
-            img6.save(morphPageRoot + pageName + fulltwo + saveType, 'PNG', quality=100)
+            draw.text((2200, 3200), pageCounterStringTwo + " Data PS", (0, 0, 0), font=font)
+            img6.save(printPageRoot + pageName + fulltwo + saveType, 'PNG', quality=100)
 
     elif counter == 2:
         img6.paste(img2, (lrMargin * counter + imageWidth * (counter - 1), tpMargin * counterTwo + imageHeight * (counterTwo - 1)))
-        img6.save(morphPageRoot + pageName + fulltwo + saveType, 'PNG', quality=100)
+        img6.save(printPageRoot + pageName + fulltwo + saveType, 'PNG', quality=100)
 
     elif counter == 3:
         img6.paste(img2, (lrMargin * counter + imageWidth * (counter - 1), tpMargin * counterTwo + imageHeight * (counterTwo - 1)))
-        img6.save(morphPageRoot + pageName + fulltwo + saveType, 'PNG', quality=100)
+        img6.save(printPageRoot + pageName + fulltwo + saveType, 'PNG', quality=100)
         counterTwo = counterTwo + 1
 
     counter = counter + 1
@@ -68,7 +68,7 @@ def pasteImages(counter, counterTwo, pageCounter, filename, file_extension):
     return counter, counterTwo
 
 
-location = morphPageRoot
+location = printPageRoot
 
 # Check whether the specified path exists or not
 isExist = os.path.exists(location)
@@ -81,10 +81,10 @@ else:
     print("The directory exists!")
 
 # Counts the number of files in the morphs folder desired
-path, dirs, fileCount = next(os.walk(morphRoot))
+path, dirs, fileCount = next(os.walk(imgRoot))
 file_count = len(fileCount)
-morphPages = get_morph_pages(file_count)
-make_morph_pages(morphPages)
+imgPages = get_print_pages(file_count)
+make_print_pages(imgPages)
 
 pageCounter = 1
 pageCounterTwo = 1
@@ -95,7 +95,7 @@ fileType = ".png"
 test_count = 1
 progress = 0
 
-for file in os.listdir(morphRoot):
+for file in os.listdir(imgRoot):
     if file.endswith(fileType):
         filename, file_extension = os.path.splitext(file)
         if counter > 3:
